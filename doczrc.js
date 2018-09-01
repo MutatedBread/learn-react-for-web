85c9e02a361319a479c2e2e238689caf9a19b8ae
chenhon199@yahoo.com <chenhon199@yahoo.com>
2018-09-01 18:10:01 +0800

    Add base url and use hash to docz config

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,7 @@
 export default {
 	typescript: true,
+	hashRouter: true,
+	base: "https://mutatedbread.github.io/react-simple-image-carousel/",
 	modifyBundlerConfig: config => {
 		config.resolve.extensions.push(".css");
 		config.module.rules.push({
